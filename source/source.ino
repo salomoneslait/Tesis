748fb3b20ffdd018d9b29c45e408f759a421fce3
salomon eslait <salomoneslait@gmail.com>
2022-09-24 15:59:04 -0500

    actualizando ui y codigo esp32

--- source/source.ino
+++ source/source.ino
@@ -1,159 +1,175 @@
-#include "esp_camera.h"
 #include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
 
-const char* ssid = "Eslait";
-const char* password = "Anadieseleda";
+//-----------------Definicion neopixel 
+int PIN = 23;
+int NUMPIXELS = 5;
 
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+uint32_t rojo = pixels.Color(150,0,0);
+uint32_t verde = pixels.Color(0,150,0);
+uint32_t azul = pixels.Color(0,0,150);
+
+//------------------Servidor Web en puerto 80---------------------
 
 WiFiServer server(80);
-bool connected = false;
-WiFiClient live_client;
+
+//---------------------Credenciales de WiFi-----------------------
+
+const char* ssid     = "Eslait";
+const char* password = "Anadieseleda";
+
+//---------------------VARIABLES GLOBALES-------------------------
+int contconexion = 0;
+
+String header; // Variable para guardar el HTTP request
+
+String estadoSalida = "off";
+
+const int salida = 2;
+
+//------------------------CODIGO HTML------------------------------
+String pagina = "<!DOCTYPE html>"
+"<html>"
+"<head>"
+"<meta charset='utf-8' />"
+"<title>Servidor Web ESP32</title>"
+"</head>"
+"<body>"
+"<center>"
+"<h1>Servidor Web ESP32</h1>"
+"<p><a href='/on'><button style='height:50px;width:100px'>ON</button></a></p>"
+"<p><a href='/off'><button style='height:50px;width:100px'>OFF</button></a></p>"
+"</center>"
+"</body>"
+"</html>";
 
 
-String index_html = "<meta charset=\"utf-8\"/>\n" \
-                    "<style>\n" \
-                    "#content {\n" \
-                    "display: flex;\n" \
-                    "flex-direction: column;\n" \
-                    "justify-content: center;\n" \
-                    "align-items: center;\n" \
-                    "text-align: center;\n" \
-                    "min-height: 100vh;}\n" \
-                    "</style>\n" \
-                    "<body bgcolor=\"#000000\"><div id=\"content\"><h2 style=\"color:#ffffff\">CiferTech LIVE</h2><img src=\"video\"></div></body>";
-
-void configCamera(){
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sscb_sda = SIOD_GPIO_NUM;
-  config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
-
-  config.frame_size = FRAMESIZE_QVGA;
-  config.jpeg_quality = 9;
-  config.fb_count = 1;
-
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-}
-
-//continue sending camera frame
-void liveCam(WiFiClient &client){
-  camera_fb_t * fb = esp_camera_fb_get();
-  if (!fb) {
-      Serial.println("Frame buffer could not be acquired");
-      return;
-  }
-  client.print("--frame\n");
-  client.print("Content-Type: image/jpeg\n\n");
-  client.flush();
-  client.write(fb->buf, fb->len);
-  client.flush();
-  client.print("\n");
-  esp_camera_fb_return(fb);
-}
-
+//---------------------------SETUP--------------------------------
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
   Serial.println("");
-  while (WiFi.status() != WL_CONNECTED) {
+  
+  pinMode(salida, OUTPUT); 
+  digitalWrite(salida, LOW);
+
+  // Conexión WIFI
+  WiFi.begin(ssid, password);
+  //Cuenta hasta 50 si no se puede conectar lo cancela
+  while (WiFi.status() != WL_CONNECTED and contconexion <50) { 
+    ++contconexion;
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  String IP = WiFi.localIP().toString();
-  Serial.println("IP address: " + IP);
-  index_html.replace("server_ip", IP);
-  server.begin();
-  configCamera();
-}
-    
-void http_resp(){
-  WiFiClient client = server.available();                           
-  if (client.connected()) {     
-      String req = "";
-      while(client.available()){
-        req += (char)client.read();
-      }
-      Serial.println("request " + req);
-      int addr_start = req.indexOf("GET") + strlen("GET");
-      int addr_end = req.indexOf("HTTP", addr_start);
-      if (addr_start == -1 || addr_end == -1) {
-          Serial.println("Invalid request " + req);
-          return;
-      }
-      req = req.substring(addr_start, addr_end);
-      req.trim();
-      Serial.println("Request: " + req);
-      client.flush();
-  
-      String s;
-      if (req == "/")
-      {
-          s = "HTTP/1.1 200 OK\n";
-          s += "Content-Type: text/html\n\n";
-          s += index_html;
-          s += "\n";
-          client.print(s);
-          client.stop();
-      }
-      else if (req == "/video")
-      {
-          live_client = client;
-          live_client.print("HTTP/1.1 200 OK\n");
-          live_client.print("Content-Type: multipart/x-mixed-replace; boundary=frame\n\n");
-          live_client.flush();
-          connected = true;
-      }
-      else
-      {
-          s = "HTTP/1.1 404 Not Found\n\n";
-          client.print(s);
-          client.stop();
-      }
-    }       
+  if (contconexion <50) {
+      // para usar con ip fija
+      IPAddress ip(192,168,1,180); 
+      IPAddress gateway(192,168,1,1); 
+      IPAddress subnet(255,255,255,0); 
+      WiFi.config(ip, gateway, subnet); 
+      
+      Serial.println("");
+      Serial.println("WiFi conectado");
+      Serial.println(WiFi.localIP());
+      server.begin(); // iniciamos el servidor
+  }
+  else { 
+      Serial.println("");
+      Serial.println("Error de conexion");
+  }
+
+  pixels.begin();
+  pixels.clear();
 }
 
-void loop() {
-  http_resp();
-  if(connected == true){
-    liveCam(live_client);
+//----------------------------LOOP----------------------------------
+
+void loop(){
+  peticion();
+
+//  int delayval = 50;
+//
+//  for(int i=0;i<NUMPIXELS;i++){
+//    pixels.setPixelColor(i,rojo);
+//    pixels.show();
+//    delay(delayval);
+//  }
+//
+//  for(int i=0;i<NUMPIXELS;i++){
+//    pixels.setPixelColor(i,verde);
+//    pixels.show();
+//    delay(delayval);
+//  }
+//
+//  for(int i=0;i<NUMPIXELS;i++){
+//    pixels.setPixelColor(i,azul);
+//    pixels.show();
+//    delay(delayval);
+//  }
+
+  
+}
+
+void peticion(){
+  WiFiClient client = server.available();   // Escucha a los clientes entrantes
+  if (client) {                             // Si se conecta un nuevo cliente
+    Serial.println("New Client.");          // 
+    String currentLine = "";                //
+    while (client.connected()) {            // loop mientras el cliente está conectado
+      if (client.available()) {             // si hay bytes para leer desde el cliente
+        char c = client.read();             // lee un byte
+        Serial.write(c);                    // imprime ese byte en el monitor serial
+        header += c;
+        if (c == '\n') {                    // si el byte es un caracter de salto de linea
+          // si la nueva linea está en blanco significa que es el fin del 
+          // HTTP request del cliente, entonces respondemos:
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // enciende y apaga el GPIO
+            if (header.indexOf("GET /?home=Habitacion&color=R") >= 0) {
+              Serial.println("GPIO on");
+              estadoSalida = "on";
+              digitalWrite(salida, HIGH);
+              pixels.setPixelColor(1,rojo);
+              pixels.show();
+            } else if (header.indexOf("GET /off") >= 0) {
+              Serial.println("GPIO off");
+              estadoSalida = "off";
+              digitalWrite(salida, LOW);
+              pixels.setPixelColor(1, pixels.Color(0, 0, 0));
+              pixels.show();
+            }else if (header.indexOf("GET /?home=Habitacion&color=A") >= 0) {
+              pixels.setPixelColor(1,azul);
+              pixels.show();
+            }else if (header.indexOf("GET /?home=Habitacion&color=V") >= 0) {
+              pixels.setPixelColor(1,verde);
+              pixels.show();
+            }
+            
+            // Muestra la página web
+            client.println(pagina);
+            
+            // la respuesta HTTP temina con una linea en blanco
+            client.println();
+            break;
+          } else { // si tenemos una nueva linea limpiamos currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // si C es distinto al caracter de retorno de carro
+          currentLine += c;      // lo agrega al final de currentLine
+        }
+      }
+    }
+    // Limpiamos la variable header
+    header = "";
+    // Cerramos la conexión
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
 }
