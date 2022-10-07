b6229bc209efa64fcf2a53d22589258948093774
salomon eslait <salomoneslait@gmail.com>
2022-10-07 17:12:14 -0500

    reformacion de backend

--- app/script.js
+++ app/script.js
@@ -11,29 +11,6 @@ formElement.addEventListener("submit", (event) => {
     let estadoJson = JSON.stringify(estado);
 
     console.log(estadoJson);
-
-    // const url = 'https://pokeapi.co/api/v2/pokemon/1/'
-
-    // fetch(url)
-    // .then(response => response.json() )
-    // .then(data => {
-    //   console.log(data)
-    // })
-    // .catch(err=>console.log(err))
-
-    // var url = 'http:192.168.1.180?home=`${habitacion}`$&range=16&Color=%237c0416&set=on';
-    // var data = {username: 'example'};
-
-    // fetch(`http://192.168.1.14/luces`, {
-    //   method: 'POST', // or 'PUT'
-    //   body: JSON.stringify(estado), // data can be `string` or {object}!
-    //   headers:{
-    //     'Content-Type': 'application/json'
-    //   },
-    //   mode: 'no-cors'
-    // }).then(res => res.json())
-    // .catch(error => console.error('Error:', error))
-    // .then(response => console.log('Success:', response));
  
     let response = fetch(`http://192.168.1.14/luces`, {
       method: 'POST',
