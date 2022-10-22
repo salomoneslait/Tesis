fa6f56c58f32a23411bb12d3089690a2c2e03671
salomon eslait <salomoneslait@gmail.com>
2022-10-22 18:59:30 -0500

    actualizando index.js

--- appExpress/index.js
+++ appExpress/index.js
@@ -9,15 +9,17 @@ app.use(express.urlencoded())
 
 var http = require('http');
 
+let backend_url = process.env.BACKEND_URL || "https://5130-2800-e2-bf80-c44-8491-9c10-1188-cf96.ngrok.io"
+
 app.get('/', function(req, res){
    res.render('form');
 });
-
+// sudo docker run -dit --env BACKEND_URL=b78a-181-206-21-114.ngrok.io -p 3030:3030 front:boton 
 app.post("/", (req, res) => {
    console.log(req.body);
 
    request
-   .post('https://5130-2800-e2-bf80-c44-8491-9c10-1188-cf96.ngrok.io')// .post('http://192.168.1.14/luces')
+   .post(`${backend_url}/luces`)
    .send(req.body)
    .set('X-API-Key', 'foobar')
    .set('Accept', 'application/json')
